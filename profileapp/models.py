b85db4d60a6fbba598574a3acbe28d45bf861a80
Sumit Nagle <76628590+Asdvamp@users.noreply.github.com>
2022-03-20 20:23:52 +0530

    V1: models.py views.py updated from previous repo, rest: first commit

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from loginapp import models as login_models
+from digischool import univeral_values as *
 
-# Create your models here.
+class USER_PROFILE_DATABASE(models.Model):
+	user_signup_db_mapping = models.OneToOneField(login_models.USER_SIGNUP_DATABASE, on_delete=models.CASCADE)
+	user_profile_photo = models.ImageField(max_length=PROFILE_PIC_PATH_LENGTH, default=os.path.join(BASE_DIR, DEFAULT_PROFILE_PHOTO))
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.template import Template, Context
 
-# Create your views here.
+from loginapp import models as login_models
+from profileapp import models as profile_models
+
+from django.middleware import csrf
+
+# NOTE: Every view function (except loginapp.views) must have session token extraction and get "user_id" which is nothing by loginapp.models.USER_SIGNUP_DATABSE.id)
+# NOTE: In every view function (except loginapp.views), session token must be validated. Else it should be redirected to loginapp.login_page.
+
+def profilePage(request):
+	# getting user_id from session token.
+	try:
+		integer_check = int(user_id)
+	except:
+		return HttpResponse('''<body><meta http-equiv="refresh" content='0; url="/login/"'/></body>''')
+
+	extract_user__user_signup_database = login_models.USER_SIGNUP_DATABASE.objects.filter(id=user_id)[0]
+	return render(request, "profile_page.html", {"user_id" : profile_models.USER_PROFILE_DATABASE.objects.filter(user_signup_db_mapping=extract_user__user_signup_database)[0]})
