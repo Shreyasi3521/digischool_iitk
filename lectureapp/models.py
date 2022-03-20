921567ccb2b7d99fb0ae0f2608813b5e177f1098
Sumit Nagle <76628590+Asdvamp@users.noreply.github.com>
2022-03-20 20:22:21 +0530

    V1: models.py updated, rest: first commit

--- lectureapp/models.py
+++ lectureapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from courseapp import models as course_models
 
-# Create your models here.
+
+class ALL_LECTURES(models.Model):
+	lecture_title = models.CharField(max_length=LECTURE_TITLE_LENGTH)
+	yt_link_unique_id = models.URLField()
+	lecture_unique_id = models.CharField(max_length=LECTURE_UNIQUE_ID) # "course_id(10):lecture_series_number(2)"
+	course_mapping = models.ForeignKey(course_models.AVAILABLE_COURSES)
