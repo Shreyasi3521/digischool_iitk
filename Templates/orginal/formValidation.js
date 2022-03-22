6c3d10c7b11fa8f3f96f454899bed3e8130142fc
Sumit Nagle <76628590+Asdvamp@users.noreply.github.com>
2022-03-22 19:05:34 +0530

    Templates modified

--- Templates/orginal/formValidation.js
+++ Templates/orginal/formValidation.js
@@ -1,7 +1,7 @@
 function first_name_validation(fcheck=false)
 {
     var MaxAllowed = 20;
-    var name = document.forms["signupform"]["first_name"].value;
+    var name = document.forms["signupform"]["fn"].value;
     name.trim();
     var name_len = name.length;
     var ErrorOccured = false;
@@ -18,7 +18,7 @@ function first_name_validation(fcheck=false)
         if (!fcheck)
         {
             alert("First name should be 1-40 charachter long and should be alphabetic.")
-            document.forms["signupform"]["first_name"].value = "";
+            document.forms["signupform"]["fn"].value = "";
         }
         
         return false;
@@ -28,7 +28,7 @@ function first_name_validation(fcheck=false)
 function last_name_validation(fcheck=false)
 {
     var MaxAllowed = 20;
-    var name = document.forms["signupform"]["last_name"].value;
+    var name = document.forms["signupform"]["ln"].value;
     name.trim();
     var name_len = name.length;
     var ErrorOccured = false;
@@ -44,7 +44,7 @@ function last_name_validation(fcheck=false)
         if (!fcheck)
         {
             alert("Last name should be 1-40 charachter long and should be alphabetic.")
-            document.forms["signupform"]["last_name"].value = "";
+            document.forms["signupform"]["ln"].value = "";
         }
         return false;
     }
@@ -53,7 +53,7 @@ function last_name_validation(fcheck=false)
 
 function class_validation(fcheck=false)
 {
-    var class_number = document.forms["signupform"]["user_class"].value;
+    var class_number = document.forms["signupform"]["cl"].value;
     var ErrorOccured = false;
     class_number = parseInt(class_number);
     if (isNaN(class_number) || class_number < 1 || class_number > 12)
@@ -66,7 +66,7 @@ function class_validation(fcheck=false)
         if (!fcheck)
         {
             alert("Class should be a number from 1-12")
-            document.forms["signupform"]["user_class"].value = "";
+            document.forms["signupform"]["cl"].value = "";
         }
         
         return false;
@@ -76,7 +76,7 @@ function class_validation(fcheck=false)
 
 function section_validation(fcheck=false)
 {
-    var section_name = document.forms["signupform"]["user_section"].value;
+    var section_name = document.forms["signupform"]["cs"].value;
     var ErrorOccured = false;
     if (section_name.length != 1)
     {
@@ -92,7 +92,7 @@ function section_validation(fcheck=false)
         if (!fcheck)
         {
             alert("Section should be either of the following, A|B|C|D|E|F")
-            document.forms["signupform"]["user_section"].value = "";
+            document.forms["signupform"]["cs"].value = "";
         }
         return false;
     }
@@ -101,7 +101,7 @@ function section_validation(fcheck=false)
 
 function rn_validation(fcheck=false)
 {
-    var roll_number = document.forms["signupform"]["r_number"].value;
+    var roll_number = document.forms["signupform"]["rn"].value;
     var ErrorOccured = false;
     roll_number_int = parseInt(roll_number);
     if (isNaN(roll_number_int) || roll_number.length < 4 || roll_number.length > 10)
@@ -114,7 +114,7 @@ function rn_validation(fcheck=false)
         if (!fcheck)
         {
             alert("Enter a valid roll number. It must be a number")
-            document.forms["signupform"]["r_number"].value = "";
+            document.forms["signupform"]["rn"].value = "";
         }
         return false;
     }
@@ -123,7 +123,7 @@ function rn_validation(fcheck=false)
 
 function contact_validation(fcheck=false)
 {
-    var contact_number = document.forms["signupform"]["contact_detail"].value;
+    var contact_number = document.forms["signupform"]["cd"].value;
     var ErrorOccured = false;
     contact_number_int = parseInt(contact_number);
     if (isNaN(contact_number_int) || contact_number.length < 10 || contact_number.length > 11)
@@ -136,7 +136,7 @@ function contact_validation(fcheck=false)
         if (!fcheck)
         {
             alert("Enter a valid Contact number. It must be a number without country code.")
-            document.forms["signupform"]["contact_detail"].value = "";
+            document.forms["signupform"]["cd"].value = "";
         }
         return false;
     }
@@ -146,7 +146,7 @@ function contact_validation(fcheck=false)
 function school_validation(fcheck=false)
 {
     var maxAllowed = 200;
-    var school_name = document.forms["signupform"]["school_name"].value;
+    var school_name = document.forms["signupform"]["sn"].value;
     if(school_name.length < 10 || school_name.length > MaxAllowed)
     {
         if (!fcheck)
@@ -160,14 +160,14 @@ function school_validation(fcheck=false)
 
 function email_validation(fcheck=false)
 {
-    var email = document.forms["signupform"]["email_address"].value;
+    var email = document.forms["signupform"]["ea"].value;
 
     if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
      {
         if (!fcheck)
         {
             alert("Enter a valid email address!")
-            document.forms["signupform"]["email_address"].value = "";
+            document.forms["signupform"]["ea"].value = "";
         }
         return false;
      }
@@ -178,11 +178,11 @@ function final_validation()
 {
     if (first_name_validation(true) && last_name_validation(true) && class_validation(true) && section_validation(true) && rn_validation(true) && contact_validation(true) && school_validation(true) && email_validation(true))
     {
-        document.forms["signupform"]["first_name"].value = document.forms["signupform"]["first_name"].value.toUpperCase()
-        document.forms["signupform"]["last_name"].value = document.forms["signupform"]["last_name"].value.toUpperCase()
-        document.forms["signupform"]["user_class"].value = document.forms["signupform"]["user_class"].value.toUpperCase()
-        document.forms["signupform"]["school_name"].value = document.forms["signupform"]["school_name"].value.toUpperCase()
-        document.forms["signupform"]["email_address"].value = document.forms["signupform"]["email_address"].value.toLowerCase()
+        document.forms["signupform"]["fn"].value = document.forms["signupform"]["fn"].value.toUpperCase()
+        document.forms["signupform"]["ln"].value = document.forms["signupform"]["ln"].value.toUpperCase()
+        document.forms["signupform"]["cs"].value = document.forms["signupform"]["cs"].value.toUpperCase()
+        document.forms["signupform"]["sn"].value = document.forms["signupform"]["sn"].value.toUpperCase()
+        document.forms["signupform"]["ea"].value = document.forms["signupform"]["ea"].value.toLowerCase()
         return true;
     }
     alert("One or more details are invalid. Please enter again!")
