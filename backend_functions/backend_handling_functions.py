5a2a8c4fa9a75cd4f7c90ae4a5f4fd7c9d04e145
Sumit Nagle <76628590+Asdvamp@users.noreply.github.com>
2022-03-25 13:26:01 +0530

    a function definition added. create it

--- backend_functions/backend_handling_functions.py
+++ backend_functions/backend_handling_functions.py
@@ -1,5 +1,6 @@
 from courseapp import models as course_models
 from . import universal_values
+from loginapp import models as login_models
 
 def auto_assign_course(user_cls, user_sec, user_category):
 	if user_category == "STUDENT":
@@ -7,13 +8,8 @@ def auto_assign_course(user_cls, user_sec, user_category):
 		class_course_mapping_entry = course_models.CLASS_COURSES_MAPPING.objects.filter(unique_id=unique_id)[0]
 		return class_course_mapping_entry
 	if user_category == "TEACHER":
-		# maybe create a null entry.
 		return None
 
-def course_instructor_assigning(teacher_entry):
-	## subject_code, coming from a csv file where we uses teachers:sc:otp mapping.	
-	course_unique_id = subject_code + teacher_entry.user_class + teacher_entry.user_section + str(OFFERING_YEAR)
-	selected_course = course_models.AVAILABLE_COURSES.objects.filter(course_id=course_unique_id)[0]
-	selected_course.course_instructor = teacher_entry
-	selected_course.save()
-
+def removing_entries():
+	pass
+	# remove loginapp.models.USER_SIGNUP_DATABASE entries if user is not verfied and time is excceded by 2 (ALLOWED_ENTRY_TIME) days.
